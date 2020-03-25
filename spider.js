8c211d0d5677ba15682c2e60de3b49ee2a518e2b
AntLouiz <luizrodrigo46@hotmail.com>
2020-03-25 19:17:40 -0300

    adding the get request of monsters category to a function

--- spider.js
+++ spider.js
@@ -1,18 +1,28 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
-var monsters_ids = [];
+const BASE_URL = 'http://www3.worldrag.com/database/'
 
-request('http://www3.worldrag.com/database/?act=mobsearch&cid=on&id=1832', function(err, res, body) {
-	if(err) console.log("Erro: "+err);
+const categories = {
+  'monsters': {'action': '?act=viewmob&displayorder=1'}
+}
 
-	var $ = cheerio.load(body);
 
-  $('#ver_monstros option').each(function(index, el) {
-    monsters_ids.push(el.attribs.value);
-	});
+function get_monsters_data() {
+  var monsters_links = [];
 
-  console.log(monsters_ids);
+  request(BASE_URL + categories['monsters']['action'], function(err, res, body) {
+    if(err) console.log("Erro: "+err);
 
-});
+    var $ = cheerio.load(body);
 
+    $('.alignment1 tbody tr td:nth-child(2) a').each(function(index, el) {
+      monsters_links.push(el.attribs.href)
+    });
+
+    console.log(monsters_links);
+  });
+}
+
+
+get_monsters_data()
