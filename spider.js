a21b68201380fce03f6e3537755b9d413b92fbcc
AntLouiz <luizrodrigo46@hotmail.com>
2020-03-25 18:46:50 -0300

    getting the monsters id's

--- spider.js
+++ spider.js
@@ -1,14 +1,18 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
+var monsters_ids = [];
+
 request('http://www3.worldrag.com/database/?act=mobsearch&cid=on&id=1832', function(err, res, body) {
 	if(err) console.log("Erro: "+err);
 
 	var $ = cheerio.load(body);
 
-	$('#AllSearchResults table:first tbody').each(function() {
-		var montersName = $('tr:first-child td:nth-child(3)').text().trim();
-		console.log(montersName);
+  $('#ver_monstros option').each(function(index, el) {
+    monsters_ids.push(el.attribs.value);
 	});
 
+  console.log(monsters_ids);
+
 });
+
